25c171583fddbdddddf27704f156b36a4e711b28
jyk0011 <jyk0013@163.com>
2020-11-29 08:23:15 +0800

    script的方式

--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 由于本项目包含多个小的demo演示，所以在src文件夹里面又建立了多个子文件夹，一个子文件夹对应一个独立的demo小项目，所以结构和默认的项目结构有点不同。  
 在main.js里面设置加载哪个demo子项目
 
-# main.js
+# 环境
+Vue3.0  
+VueRouter 4.0  
+* 最新稳定版
+$ npm install vue@next
+
+* 安装 
+npm install -g @vue/cli@next
+
+ant Design Vue2.x
+* 安装 
+$ npm install ant-design-vue --save
+
+
+# main.js 加载子项目的方法
 通过比较笨的注释的方式，注释掉不需要运行的demo，只保留一个想要运行的demo
 
 # 路由demo
@@ -13,6 +27,10 @@
 ## 用户中心的路由 /src/router-userCenter
 使用用户中心的通用设置，验证路由的各种使用方式
 
+视频演示：
+
 ## 仿某乎的路由 /src/router-zhihu
 仿某乎的路由，仿制一套路由设置
+
+视频演示：https://www.zhihu.com/zvideo/1314326340745904128 
 
--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 
 // 一开始的路由尝试
-import App from './router-test/App.vue'
-import router from './router-test/router'
-import store from './router-test/store'
+// import App from './router-test/App.vue'
+// import router from './router-test/router'
+// import store from './router-test/store'
 
 // 用户中心的路由演示
-// import App from './router-userCenter/App.vue'
-// import router from './router-userCenter/router'
-// import store from './router-userCenter/store'
+import App from './router-userCenter/App.vue'
+import router from './router-userCenter/router'
+import store from './router-userCenter/store'
 
 // 仿某乎的路由演示
 // import App from './router-zhihu/App.vue'
